d172906d8a407775d28fab4637a24882b788c296
dominhit215@gmail.com <dominhit215@gmail.com>
2025-08-08 18:23:16 +0700

    setup eureka server

--- banking-core-service-registry/src/main/java/com/gahoccode/finance/banking_core_service_registry/BankingCoreServiceRegistryApplication.java
+++ banking-core-service-registry/src/main/java/com/gahoccode/finance/banking_core_service_registry/BankingCoreServiceRegistryApplication.java
@@ -2,8 +2,10 @@ package com.gahoccode.finance.banking_core_service_registry;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class BankingCoreServiceRegistryApplication {
 
 	public static void main(String[] args) {
